a31551798447b5d0fc4a5d7601e01a986b6cab44
yongyu2017 <286016906@qq.com>
2019-05-14 14:37:16 +0800

    1

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# popupBox
-111
+## 返璞归真
+
+popupBox是一款web弹层组件，该组件有pc版本和wap版本。pc版本兼容了包括IE6在内的所有主流浏览器。
+pc版本和wap版本都依赖第三方库，jquery.js、zepto.js。
+
+## 快速上手
+
+获得 popupBox 后，以pc版本为例，你只需要引入下述几个文件：
+
+```
+./css/popupBox.css
+./js/jquery.js
+./js/popupBox.js
+```
+
+组件的参数、内置方法在js文件最上面注释代码。pc端和wap端的文档有点不相同，请各位看官详细阅读。
---
+++ js/popupBox_m.js
@@ -0,0 +1,238 @@
+/**
+ * 基础参数：
+ * className层样式类名。类型：String，默认：''
+ * shadeClassName遮罩样式类名。类型：String，默认：''
+ * title标题。类型：String，默认：'信息'
+ * content内容。类型：String/DOM，默认：''
+ * btn提示按钮，类型：Array，默认：[]
+ * closeBtn右上关闭，类型：Boolean，默认：true
+ * shade遮罩，类型：Boolean，默认：true
+ * shadeClose是否点击遮罩关闭，类型：Boolean，默认：true
+ * time自动关闭，类型：Number，默认：0；默认不会自动关闭。当你想自动关闭时，可以time: 5000，即代表5秒后自动关闭，注意单位是毫秒（1秒=1000毫秒）
+ * success层弹出后的成功回调方法，该回调携带两个参数，分别为当前层索引、当前层DOM对象
+ * yes确定按钮回调方法，该回调携带两个参数，分别为当前层索引、当前层DOM对象；默认会自动触发关闭，如果不想关闭，return false即可
+ * cancel右上角关闭按钮触发的回调，该回调携带两个参数，分别为当前层索引、当前层DOM对象；默认会自动触发关闭，如果不想关闭，return false即可
+ * end层销毁后触发的回调
+ * 内置方法：
+ * popupBox.open(options)
+ * popupBox.msg(content, options)
+ * popupBox.load(options)
+ * popupBox.close(index)
+ * popupBox.closeAll()
+ * **/
+
+(function (window, document, undefined) {
+
+    var ready = {
+        index: 0,  //弹窗个数
+        timer: {},  //倒计时关闭存储
+        end: {}  //弹窗关闭回调
+    }
+    var popupBox = {
+        open: function (options) {
+            var o = new PopupBox(options);
+            return o.boxIndex;
+        },
+        msg: function (content, options) {
+            var defaultConfig= {
+                className: 'customLayer-msg',
+                shadeClassName: 'customLayer-msgShade',
+                shade: false,
+                shadeClose: false,
+                closeBtn: false,
+                title: false,
+                content: '<div class="png"></div><div class="pngCon">'+ content +'</div>',
+                btn: [],
+                time: 3000
+            };
+            if(options){
+                var userConfig= options;
+                userConfig.className= 'customLayer-msg '+ (userConfig.className? userConfig.className: '');
+                userConfig.shadeClassName= 'customLayer-msgShade '+ (userConfig.shadeClassName? userConfig.shadeClassName: '');
+                $.extend(defaultConfig, userConfig);
+            }
+            var o= new PopupBox(defaultConfig);
+            return o.boxIndex;
+        },
+        load: function (options) {
+            var defaultConfig= {
+                className: 'customLayer-load',
+                shadeClassName: 'customLayer-loadShade',
+                shade: true,
+                shadeClose: false,
+                closeBtn: false,
+                title: false,
+                content: '<div class="loading"></div>',
+                btn: [],
+                time: 3000
+            };
+            if(options){
+                var userConfig= options;
+                userConfig.className= 'customLayer-load '+ (userConfig.className? userConfig.className: '');
+                userConfig.shadeClassName= 'customLayer-loadShade '+ (userConfig.shadeClassName? userConfig.shadeClassName: '');
+                $.extend(defaultConfig, userConfig);
+            }
+            var o= new PopupBox(defaultConfig);
+            return o.boxIndex;
+        },
+        close: function (index) {
+            var elem = $('#customLayer' + index), contentDom = elem.find('.customLayer-content').children(), shadeDom = $('#customLayer-shade' + index);
+            clearTimeout(ready.timer[index]);
+            delete ready.timer[index];
+            shadeDom.remove();
+            if (elem.attr('contype') == 'Object') {
+                elem.find('.customLayer-setwin').remove();
+                elem.find('.customLayer-title').remove();
+                elem.find('.customLayer-btn').remove();
+                contentDom.parents('.customLayer-content').unwrap();
+                contentDom.unwrap().css('display', 'none');
+            } else {
+                elem.remove();
+            }
+            //弹窗结束回调
+            typeof ready.end[index] === 'function' && ready.end[index]();
+            delete ready.end[index];
+        },
+        closeAll: function () {
+            var _this= this;
+            $('.customLayer').each(function () {
+                var Index= Number($(this).attr('id').replace('customLayer', ''));  //获取弹窗索引标识
+                _this.close(Index)
+            })
+        }
+    }
+
+    var PopupBox = function (options) {
+        var that = this;
+        that.config = {
+            className: '',
+            shadeClassName: '',
+            shade: true,
+            shadeClose: true,
+            closeBtn: true,
+            title: '信息',  //title值为false，则不显示标题
+            content: '',
+            btn: [],
+            time: 0
+        }
+        $.extend(that.config, options);
+        that.view();
+    }
+    PopupBox.prototype.view = function () {
+        var that = this,
+            config = that.config,
+            noTitle = config.title ? '' : 'customLayer-noTitle',
+            initialZindex = 998,
+            contype = config.content instanceof Object ? 'Object' : 'String';  //content类型
+
+        that.boxIndex = ++ready.index;
+        var titleStr = '<div class="customLayer-title">' + config.title + '</div>',
+            shadeStr = '<div class="customLayer-shade '+ config.shadeClassName +'" id="customLayer-shade'+ that.boxIndex +'" style="z-index: ' + (initialZindex + that.boxIndex- 1) + ';"></div>',
+            closeBtnStr = '<div class="customLayer-setwin">' +
+                '<a href="javascript:void(0);" class="customLayer-close"></a>' +
+                '</div>',
+            btnStr = '';
+        if (config.btn.length > 0) {
+            btnStr += '<div class="customLayer-btn">';
+            for (var i = 0; i < (config.btn.length > 2 ? 2 : config.btn.length); i++) {
+                btnStr += '<a href="javascript:void(0);" class="customLayer-btn' + i + '">' + config.btn[i] + '</a>';
+            }
+            btnStr += '</div>';
+        }
+
+        //config.content，判断类型
+        if (contype == 'Object') {
+            config.content.css('display', 'block').wrap('<div class="customLayer ' + config.className + ' ' + noTitle + '" id="customLayer' + that.boxIndex + '" contype="' + contype + '" style="z-index: ' + (initialZindex + that.boxIndex) + ';"><div class="customLayer-content"></div></div>');
+            //标题
+            if (config.title) {
+                config.content.parents('.customLayer').prepend(titleStr);
+            }
+            //关闭按钮
+            if (config.closeBtn) {
+                config.content.parents('.customLayer').prepend(closeBtnStr);
+            }
+            //操作按钮
+            if (config.btn.length > 0) {
+                config.content.parents('.customLayer').append(btnStr);
+            }
+        } else {
+            var domStr = '<div class="customLayer ' + config.className + ' ' + noTitle + '" id="customLayer' + that.boxIndex + '" contype="' + contype + '" style="z-index: ' + (initialZindex + that.boxIndex) + '">';
+            //标题
+            if (config.title) {
+                domStr += titleStr;
+            }
+            //关闭按钮
+            if (config.closeBtn) {
+                domStr += closeBtnStr;
+            }
+            domStr += '<div class="customLayer-content">';
+            domStr += config.content;
+            domStr += '</div>';
+            //操作按钮
+            if (config.btn.length > 0) {
+                domStr += btnStr;
+            }
+            domStr += '</div>';
+
+            $('body').append(domStr);
+        }
+        //遮罩
+        if (config.shade) {
+            $('body').append(shadeStr);
+        }
+
+        var elem = $('#customLayer' + that.boxIndex);
+
+        //弹窗弹出后的成功回调方法
+        if (typeof config.success === 'function') {
+            config.success(that.boxIndex, elem);
+        }
+
+        that.action(config, elem)
+    }
+    PopupBox.prototype.action = function (config, elem) {
+        var that = this, closeBtnDom = elem.find('a.customLayer-close'), shadeDom = $('#customLayer-shade' + that.boxIndex),
+            confirmBtnDom = elem.find('.customLayer-btn0'), cancelBtnDom = elem.find('.customLayer-btn1');
+
+        //弹窗结束回调
+        config.end && (ready.end[that.boxIndex] = config.end);
+
+        //点击遮罩关闭弹窗
+        if (config.shadeClose) {
+            shadeDom.off('click').on('click', function () {
+                popupBox.close(that.boxIndex);
+            });
+        }
+        //关闭按钮
+        if (config.closeBtn) {
+            closeBtnDom.on('click', function () {
+                var isClose = config.cancel && config.cancel(that.boxIndex, elem);  //当config.cancel方法里有返回值return false，则不关闭弹窗
+                if (isClose == undefined) {
+                    popupBox.close(that.boxIndex);
+                }
+            })
+        }
+        //自动删除隐藏
+        if (config.time) {
+            ready.timer[that.boxIndex] = setTimeout(function () {
+                popupBox.close(that.boxIndex);
+            }, config.time);
+        }
+        //确认和取消按钮
+        if (config.btn.length > 0) {
+            confirmBtnDom.on('click', function () {
+                var isClose = config.yes && config.yes(that.boxIndex, elem);  //当config.yes方法里有返回值return false，则不关闭弹窗
+                if (isClose == undefined) {
+                    popupBox.close(that.boxIndex);
+                }
+            })
+            cancelBtnDom.on('click', function () {
+                popupBox.close(that.boxIndex);
+            })
+        }
+
+    }
+
+    window.popupBox = popupBox;
+
+})(window, document);
